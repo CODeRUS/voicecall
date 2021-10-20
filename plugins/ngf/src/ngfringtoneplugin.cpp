9151d7369ed33dd8166b55fedd762c29f580aa7c
coderus <coderusinbox@gmail.com>
2021-10-21 00:28:58 +0300

    personal ringtones hack

--- plugins/ngf/src/ngfringtoneplugin.cpp
+++ plugins/ngf/src/ngfringtoneplugin.cpp
@@ -22,10 +22,43 @@
 #include "ngfringtoneplugin.h"
 
 #include <voicecallmanagerinterface.h>
+#include <mlite5/MGConfItem>
 
 #include <NgfClient>
 
+#include <QDir>
+#include <QFileInfo>
 #include <QtPlugin>
+
+namespace {
+
+const QString personalName = QStringLiteral("personal_ringtone");
+const QString importantName = QStringLiteral("important_ringtone");
+const QString confPath = QStringLiteral("/apps/personal-ringtones/%1");
+
+template <typename T>
+T getConfValue(const QString &key, const T &def)
+{
+    QVariant value = MGConfItem(confPath.arg(key)).value();
+    if (value.isNull()) {
+        return def;
+    }
+    if (!value.canConvert<T>()) {
+        return def;
+    }
+    return value.value<T>();
+}
+
+QString getPersonalRingtone(const QString &number)
+{
+    QString ringtone = getConfValue(number, QString());
+    if (!ringtone.isEmpty() && QFileInfo::exists(ringtone)) {
+        return ringtone;
+    }
+    return QString();
+}
+
+} // namespace
 
 class NgfRingtonePluginPrivate
 {
@@ -33,7 +66,7 @@ class NgfRingtonePluginPrivate
 
 public:
     NgfRingtonePluginPrivate(NgfRingtonePlugin *q)
-        : q_ptr(q), manager(NULL), currentCall(NULL), activeCallCount(0), ngf(NULL), ringtoneEventId(0)
+        : q_ptr(q), manager(NULL), currentCall(NULL), activeCallCount(0), ngf(NULL)
     { /* ... */ }
 
     NgfRingtonePlugin *q_ptr;
@@ -43,18 +76,25 @@ public:
     int activeCallCount;
 
     Ngf::Client *ngf;
-    quint32 ringtoneEventId;
+    QString lastEvent;
 };
 
 NgfRingtonePlugin::NgfRingtonePlugin(QObject *parent)
     : AbstractVoiceCallManagerPlugin(parent), d_ptr(new NgfRingtonePluginPrivate(this))
 {
     TRACE
+
+    qDebug() << Q_FUNC_INFO << "Personal ringtones plugin created";
+
+    qsrand(QDateTime::currentMSecsSinceEpoch());
 }
 
 NgfRingtonePlugin::~NgfRingtonePlugin()
 {
     TRACE
+
+    qDebug() << Q_FUNC_INFO << "Personal ringtones plugin deleted";
+
     delete this->d_ptr;
 }
 
@@ -67,6 +107,7 @@ QString NgfRingtonePlugin::pluginId() const
 bool NgfRingtonePlugin::initialize()
 {
     TRACE
+    qDebug() << Q_FUNC_INFO << "Personal ringtones plugin initialize";
     Q_D(NgfRingtonePlugin);
     d->ngf = new Ngf::Client(this);
     return true;
@@ -75,6 +116,7 @@ bool NgfRingtonePlugin::initialize()
 bool NgfRingtonePlugin::configure(VoiceCallManagerInterface *manager)
 {
     TRACE
+    qDebug() << Q_FUNC_INFO << "Personal ringtones plugin configure";
     Q_D(NgfRingtonePlugin);
     d->manager = manager;
 
@@ -90,6 +132,7 @@ bool NgfRingtonePlugin::configure(VoiceCallManagerInterface *manager)
 bool NgfRingtonePlugin::start()
 {
     TRACE
+    qDebug() << Q_FUNC_INFO << "Personal ringtones plugin start";
     Q_D(NgfRingtonePlugin);
 
     QObject::connect(d->manager, SIGNAL(voiceCallAdded(AbstractVoiceCallHandler*)), SLOT(onVoiceCallAdded(AbstractVoiceCallHandler*)));
@@ -102,18 +145,21 @@ bool NgfRingtonePlugin::start()
 bool NgfRingtonePlugin::suspend()
 {
     TRACE
+    qDebug() << Q_FUNC_INFO << "Personal ringtones plugin suspend";
     return true;
 }
 
 bool NgfRingtonePlugin::resume()
 {
     TRACE
+    qDebug() << Q_FUNC_INFO << "Personal ringtones plugin resume";
     return true;
 }
 
 void NgfRingtonePlugin::finalize()
 {
     TRACE
+    qDebug() << Q_FUNC_INFO << "Personal ringtones plugin finalize";
 }
 
 void NgfRingtonePlugin::onVoiceCallAdded(AbstractVoiceCallHandler *handler)
@@ -147,14 +193,19 @@ void NgfRingtonePlugin::onVoiceCallStatusChanged(AbstractVoiceCallHandler *handl
         if (d->currentCall == handler) {
             d->currentCall = NULL;
 
-            if (d->ringtoneEventId) {
+            if (!d->lastEvent.isEmpty())
+            {
                 DEBUG_T("Stopping ringtone");
-                d->ngf->stop("ringtone");
-                d->ringtoneEventId = 0;
+                d->ngf->stop(d->lastEvent);
+                d->lastEvent = QString();
             }
         }
-    } else if (!d->ringtoneEventId && !d->currentCall) {
+    } else if (d->lastEvent.isEmpty() && !d->currentCall) {
         d->currentCall = handler;
+        QString number = handler->lineId();
+        qDebug() << Q_FUNC_INFO
+            << "Incoming from:"
+            << number;
 
         QMap<QString, QVariant> props;
         if (d->activeCallCount > 1) {
@@ -165,8 +216,72 @@ void NgfRingtonePlugin::onVoiceCallStatusChanged(AbstractVoiceCallHandler *handl
             props.insert("type", "voip");
         }
 
-        d->ringtoneEventId = d->ngf->play("ringtone", props);
-        DEBUG_T("Playing ringtone, event id: %u", d->ringtoneEventId);
+        int match = getConfValue(QStringLiteral("match"), 0);
+        if (match > 0) {
+            QString numbersItem = getConfValue(QStringLiteral("numbers"), QString());
+            QStringList numbers = numbersItem.split(QChar(u';'));
+            for (const QString &num : numbers) {
+                if (num.right(match) == number.right(match)) {
+                    number = num;
+                    qDebug() << Q_FUNC_INFO
+                        << "Matching number:"
+                        << number;
+                    break;
+                }
+            }
+        }
+
+        QString ringtone = getPersonalRingtone(number);
+        qDebug() << Q_FUNC_INFO
+            << "Ringtone for:"
+            << number
+            << ringtone;
+        if (ringtone == QLatin1String("muted")) {
+            d->lastEvent = QString();
+            qDebug("Ignoring ringtone");
+            return;
+        } else if (ringtone.isEmpty()) {
+            bool isRandom = getConfValue(QStringLiteral("random"), false);
+            if (isRandom) {
+                QString randomPath = getConfValue(QStringLiteral("randomPath"), QStringLiteral("/usr/share/sounds/jolla-ringtones/stereo"));
+                QDir randomDir(randomPath);
+                if (randomDir.exists()) {
+                    QStringList ringtones = randomDir.entryList(QDir::Files, QDir::Name);
+                    if (ringtones.length() > 0) {
+                        ringtone = ringtones.at(qrand() % ringtones.length());
+                        ringtone = randomDir.absoluteFilePath(ringtone);
+                        qDebug() << Q_FUNC_INFO
+                            << "Random ringtone:"
+                            << ringtone;
+                    }
+                }
+            }
+        }
+
+        if (!ringtone.isEmpty()) {
+            props.insert("audio", ringtone);
+        }
+
+        d->lastEvent = personalName;
+
+        QStringList importantNumbers = getConfValue(QStringLiteral("important"), QString()).split(QChar(u';'));
+        if (match > 0) {
+            for (const QString &importantNumber : importantNumbers) {
+                if (importantNumber.right(match) == number.right(match)) {
+                    qDebug() << Q_FUNC_INFO
+                        << "Matching important number:"
+                        << importantNumber;
+                    d->lastEvent = importantName;
+                    break;
+                }
+            }
+        } else if (importantNumbers.contains(number)) {
+            d->lastEvent = importantName;
+        }
+        qDebug() << props;
+
+        quint32 eventId = d->ngf->play(d->lastEvent, props);
+        qDebug("Playing ringtone, event (%s) id: %u", d->lastEvent.toLatin1().constData(), eventId);
     }
 }
 
@@ -178,10 +293,10 @@ void NgfRingtonePlugin::onVoiceCallDestroyed()
     if (d->currentCall == sender()) {
         d->currentCall = NULL;
 
-        if (d->ringtoneEventId) {
+        if (!d->lastEvent.isEmpty()) {
             DEBUG_T("Stopping ringtone");
             d->ngf->stop("ringtone");
-            d->ringtoneEventId = 0;
+            d->lastEvent = QString();
         }
     }
 
@@ -193,9 +308,10 @@ void NgfRingtonePlugin::onSilenceRingtoneRequested()
 {
     TRACE
     Q_D(NgfRingtonePlugin);
-    if (d->ringtoneEventId) {
+    if (!d->lastEvent.isEmpty())
+    {
         DEBUG_T("Pausing ringtone due to silence");
-        d->ngf->pause("ringtone");
+        d->ngf->pause(d->lastEvent);
     }
 }
 
